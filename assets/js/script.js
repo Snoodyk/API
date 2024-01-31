1f8e913fba6ce45a5b2b9aa5f15e5285712598e4
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-01-31 11:48:51 +0000

    finished project

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,9 @@ document.getElementById("submit").addEventListener("click", e => postForm(e));
 
 function processOptions(form){
     	let optArray =[]
-        for (let entry of form.entries()){
-            if(entry[0]==="options"){
-                optArray.push(entry[1]);
+        for (let e of form.entries()){
+            if(e[0]==="options"){
+                optArray.push(e[1]);
             }
 }
     form.delete("options");
@@ -31,11 +31,13 @@ async function postForm(e){
             if (response.ok){
                 displayErrors(data)
             } else {
-                throw new Error (data,error);
+                displayException(data);
+                throw new Error (data.error);
             }
 }
 
 function displayErrors(data){
+    let results="";
     let heading = `JsHint results for ${data.file}`;
 
     if (data.total_errors === 0 ){
@@ -66,6 +68,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data)
         throw new Error(data.error);
     }
 
@@ -76,6 +79,19 @@ function displayStatus(data) {
     let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
+
+function displayException(data) {
+
+    let heading = `<div class="error-heading">An Exception Occurred</div>`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
